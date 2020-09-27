84468543dd65f2748ebb68d7a4f17424d5375ff0
khabdrick <muhamzyali@gmail.com>
2020-09-27 21:42:11 +0100

    loop through the items in github API

--- most_starred2.py
+++ most_starred2.py
@@ -2,5 +2,18 @@ import pandas as pd
 import requests 
 from datetime import datetime
 df = pd.DataFrame(columns=[ 'name', 'URL', 'created_date', 'number_of_stars'])
-results = requests.get('https://api.github.com/search/repositories?q=created:%22%3E2020-08-30%22&sort=stars&order=desc&per_page=100').json()
+results = requests.get('https://api.github.com/search/repositories?q=created:%22%3E2020-08-30%22& \
+sort=stars&order=desc&per_page=100').json()
 
+for repo in results['items'][0:100]:
+        d_tmp = {
+                'name': repo['name'],
+                'URL': repo['html_url'],
+                'created_date': datetime.strptime(repo['created_at'], '%Y-%m-%dT%H:%M:%SZ'),
+
+
+                'number_of_stars': repo['stargazers_count']}
+       
+
+
+        print (d_tmp)
